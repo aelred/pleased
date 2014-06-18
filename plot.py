c87de3e64a29faf5d9177bd281436f52e8a0e5e7
Felix Chapman <aelred717@gmail.com>
2014-06-18 15:57:44 +0100

    Fix displaying multiple stimuli

--- plot.py
+++ plot.py
@@ -4,7 +4,8 @@ from collections import Counter
 
 def plot_plant_data(plant_data):
 	plt.plot(plant_data.readings)
-	plt.axvline([s.time for s in plant_data.stimuli])
+	for s in plant_data.stimuli:
+		plt.axvline(s.time)
 
 def plot_datapoints(datapoints):
 	map(plot_datapoint, datapoints)
