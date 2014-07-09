6918026e3959f2cf615a215ea8b1628a10b467a1
Felix Chapman <aelred717@gmail.com>
2014-07-09 11:30:06 +0100

    Reformat whitespace to match tab size of 4 spaces

--- plot.py
+++ plot.py
@@ -6,9 +6,9 @@ import datapoint as datap
 
 
 def plant_data(pd):
-	plt.plot(pd.readings)
-	for s in pd.stimuli:
-		plt.axvline(s.time)
+    plt.plot(pd.readings)
+    for s in pd.stimuli:
+        plt.axvline(s.time)
 
 def datapoints(X, y):
     [datapoint(xx, yy) for xx, yy in zip(X, y)]
@@ -19,19 +19,19 @@ def datapoint(xx, yy, stim_line=True):
         plt.axvline(-datap.window_offset)
 
 def show():
-	plt.show()
+    plt.show()
 
 def plant_data_save(plant_list, path="plant_plots"):
-	for p in plant_list:
-		plant_data(p)
-		plt.savefig(os.path.join(path, "%s.jpg" % p.name))
-		plt.clf()
+    for p in plant_list:
+        plant_data(p)
+        plt.savefig(os.path.join(path, "%s.jpg" % p.name))
+        plt.clf()
 
 def datapoints_save(X, y, path="plots"):
-	type_count = Counter()
+    type_count = Counter()
 
-	for xx, yy in zip(X, y):
-		type_count[yy] += 1
-		datapoint(xx, yy)
-		plt.savefig(os.path.join(path, "%s_%d.png" % (yy, type_count[yy])))
-		plt.clf()
+    for xx, yy in zip(X, y):
+        type_count[yy] += 1
+        datapoint(xx, yy)
+        plt.savefig(os.path.join(path, "%s_%d.png" % (yy, type_count[yy])))
+        plt.clf()
