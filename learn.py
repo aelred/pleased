9a72272e6c42a182aaff236ac4fab079d70606d8
Felix Chapman <aelred717@gmail.com>
2014-07-11 15:46:00 +0100

    Move legend on scatter plot to better show plot

--- learn.py
+++ learn.py
@@ -124,7 +124,7 @@ class Classifier:
         axes.set_ylabel('LDA Basis vector 2')
         if title:
             axes.set_title(title)
-        axes.legend()
+        axes.legend(bbox_to_anchor=(1.12, 1.0))
         fig.show()
 
     def plot(self, title=None, split=True):
